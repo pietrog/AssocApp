9d9ff1232b9db06eaf6f5cad915d4b3e17a13cb5
Pierre Gaulard <pierre.gaulard@gmail.com>
2018-11-07 22:32:16 +0100

    Front: Add NewEvent vue, adding a new event directly on the calendar

--- front/src/components/Calendar.vue
+++ front/src/components/Calendar.vue
@@ -7,15 +7,17 @@
     <calendar-view
       :show-date="showDate"
       class="theme-default holiday-us-traditional holiday-us-official"
-      v-bind:events="eevents"
+      v-bind:events="events"
       >
-      <calendar-view-header />
+      <calendar-view-header slot="header" slot-scope="t" :header-props="t.headerProps" @input="setShowDate"/>
     </calendar-view>
   </div>
+  <new-event v-bind:events="events"/>
 </div>
 </template>
 
 <script>
+import NewEvent from './NewEvent';
 import VueSimpleCalendar from 'vue-simple-calendar';
 require("vue-simple-calendar/static/css/default.css");
 require("vue-simple-calendar/static/css/holidays-us.css");
@@ -23,21 +25,33 @@ require("vue-simple-calendar/static/css/holidays-us.css");
 export default {
     name: 'calendar',
     components: {
-	VueSimpleCalendar
+	VueSimpleCalendar,
+	NewEvent
     },
     data: function() {
 	return {
 	    showDate: new Date(),
-	    eevents:  
+	    events:  
 	    [
 		{
 		    startDate: "2018-11-08 00:00:00",
 		    endDate: "2018-11-09 12:00:00",
 		    title: "test",
 		    id: "sdkjhvliuzehv"
+		},
+		{
+		    startDate: "2018-11-11 13:00:00",
+		    endDate: "2018-11-11 18:00:00",
+		    title: "test2",
+		    id: "sdkjhvliuzehv"
 		}
 	    ]
 	}
+    },
+    methods: {
+	setShowDate(d) {
+	    this.showDate = d;
+	},
     }
 }
 </script>
---
+++ front/src/components/NewEvent.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  COUCOU
+  <table>
+    <tr>
+      <td>Nom de l'évènement</td>
+      <td><input v-model="event.title" /></td>
+    </tr>
+    <tr>
+      <td>Date de début</td>
+      <td><input v-model="event.startDate" type="date"/></td>
+    </tr>
+    <tr>
+      <td>Date de fin</td>
+      <td><input v-model="event.endDate" type="date"/></td>
+    </tr>    
+  </table>
+  <button v-on:click="saveEvent">Créer</button>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'new-event',
+    data: function() {
+	return {
+	    event: {}
+	}
+    },
+    props: ['events'],
+    methods: {
+	saveEvent: function() {
+	    this.events.push(this.event);
+	}
+    }
+}
+</script>
+
+<style scoped>
+</style>
