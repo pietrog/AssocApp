13cc5f5a32183d9cc033cedbd2ce49a37184548a
pietro <pierre.gaulard@gmail.com>
2018-12-07 17:05:03 +0100

    WebPack: configuration file for webpack is now for both prod and dev, everything in only one file

--- build/build.js
+++ build/build.js
@@ -2,41 +2,65 @@
 
 const rm = require('rimraf') // remove node module folder
 const chalk = require('chalk') // terminal string styling
+const ora = require('ora')  // nice displaying of a text (animation)
+const path = require('path');
+
 const webpack = require('webpack')
-const config = require('../config');
 
-module.exports = (webpackConfig, spinner) => {
-    rm(config.build.assetsRoot, err => {
+if ( process.argv.length === 3 && (process.argv[2] === 'development' || process.argv[2] === 'production')) {
+    process.env['NODE_ENV'] = process.argv[2];
+}
+else {
+    console.log(chalk.redBright('Expects mode, development or production'));
+    console.log(chalk.red('When launching build.js, development or production is expected\n'));
+    process.exit(1);
+}
 
+const output_folder = path.resolve(__dirname, '../dist/');
+const input_folder = path.resolve(__dirname, '../front/');
+
+
+const config = require('./webpack.config');
+
+const spinner = ora('building for ' + process.env.NODE_ENV);
+spinner.start();
+
+
+//clean dist folder before build
+rm(output_folder, err => {
+
+    if (err) {
+	spinner.stop()
+	console.log(chalk.red('Error while cleaning the dist folder: ' + err));
+	process.exit(1);
+    }
+
+    //launch webpack
+    webpack(config, (err, stats) => {
+
+	//stop the spinner, build is finished
+	spinner.stop()
+	
 	if (err) {
-	    throw err;
+	    console.log(chalk.redBright('Error while building: ' + err));
+	    process.exit(1);
 	}
 
-	//launch webpack
-	webpack(webpackConfig, (err, stats) => {
-
-	    //stop the spinner, build is finished
-	    spinner.stop()
-	    if (err) {
-		throw err;
-	    }
-
-	    
-	    process.stdout.write(stats.toString({
-		colors: true,
-		modules: false,
-		children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
-		chunks: false,
-		chunkModules: false
-	    }) + '\n\n')
-	    
-	    if (stats.hasErrors()) {
-		console.log(chalk.red('  Build failed with errors.\n'))
-		process.exit(1)
-	    }
-	    
-	    console.log(chalk.cyan('  Build complete.\n'))
-	})
+	
+	process.stdout.write(stats.toString({
+	    colors: true,
+	    modules: false,
+	    children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+	    chunks: false,
+	    chunkModules: false
+	}) + '\n\n')
+	
+	if (stats.hasErrors()) {
+	    console.log(chalk.red('  Build failed with errors.\n'))
+	    process.exit(1)
+	}
+	
+	console.log(chalk.cyan('  Build complete.\n'))
     })
-};
+})
 
---
+++ build/webpack.config.js
@@ -0,0 +1,108 @@
+'use strict'
+const path = require('path');
+const config = require('../config');
+const { VueLoaderPlugin } = require('vue-loader');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+
+//const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const NODE_ENV = process.env.NODE_ENV;
+const output_folder = path.resolve(__dirname, '..', 'dist');
+const input_folder = path.resolve(__dirname, '..', 'front');
+
+const isProd = () => {
+    return (NODE_ENV === 'production') ? true : false;
+};
+
+
+/*
+ * Css extraction for easier css splitting
+ */
+const extractCSS = new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // both options are optional
+    filename: 'css/[name].[hash].css',
+    chunkFilename: 'css/[id].[hash].css',
+});
+
+const extractHTML = new HtmlWebpackPlugin({
+    template: input_folder+'/index.html',
+})
+
+//new BundleAnalyzerPlugin()
+
+module.exports = {
+    mode: NODE_ENV,
+    //devtool: 'inline-source-map',
+    //watch: true,
+    context: input_folder,
+    entry: {
+	index: './src/main.js',
+    },
+    resolve: {
+	extensions: ['.js', '.vue', '.json'],
+	alias: {
+	    'vue$': 'vue/dist/vue.esm.js',
+	    '@': path.resolve(input_folder, 'src')
+	}
+    },
+    optimization: {
+	runtimeChunk: false,
+	splitChunks: {
+	    chunks: "all" //Taken from https://gist.github.com/sokra/1522d586b8e5c0f5072d7565c2bee693
+	}
+    },
+    plugins: [
+	new VueLoaderPlugin(),
+	new FriendlyErrorsPlugin(),
+	//new webpack.HotModuleReplacementPlugin(),
+	extractCSS,
+	extractHTML
+    ],
+    module: {
+	rules: [
+	    {
+		test: /\.vue$/,
+		loader: 'vue-loader',
+	    },
+	    {
+		test: /\.js$/,
+		exclude: /(node_modules|bower_components)/,
+		use: { loader: 'babel-loader' }
+	    },
+	    {
+		test: /\.(sa|sc|cs)s$/,
+		use: [
+		    {
+			loader: isProd()? MiniCssExtractPlugin.loader: 'style-loader'
+		    },
+		    'css-loader',
+		]
+	    },
+	    {
+		test: /\.(png|jpg|gif)$/,
+		loader: 'file-loader',
+		query: {
+		    name: '[name].[ext]?[hash]',
+		    useRelativePath: !isProd()
+		}
+	    }    	    
+	]
+    },
+    node: {
+	// prevent webpack from injecting useless setImmediate polyfill because Vue
+	// source contains it (although only uses it if it's native).
+	setImmediate: false,
+	// prevent webpack from injecting mocks to Node native modules
+	// that does not make sense for the client
+	dgram: 'empty',
+	fs: 'empty',
+	net: 'empty',
+	tls: 'empty',
+	child_process: 'empty'
+    }    
+}
